fc48b23e59e1ffc8c5f5603b14b7128b33db8773
Fair Analytics <158599661+dgnaglo@users.noreply.github.com>
2024-09-23 16:22:58 +0200

    changes choosed

--- R/dbm_config_mod.R
+++ R/dbm_config_mod.R
@@ -11,14 +11,24 @@ dbConfigUI <- function(id) {
   ns <- shiny::NS(id)
   bs4Dash::box(
     title = "Database Configuration",
-    shiny::textInput(ns("db_name"), "Database Name", value = Sys.getenv('TH_DATABASE')),
-    shiny::passwordInput(ns("db_host"), "Host", value = Sys.getenv('TH_HOST')),
-    shiny::numericInput(ns("db_port"), "Port", value = Sys.getenv('TH_PORT')),
-    shiny::passwordInput(ns("db_user"), "Username", value = Sys.getenv('TH_DB_USERNAME')),
-    shiny::passwordInput(ns("db_password"), "Password", value = Sys.getenv('TH_DB_PASSWORD') ),
-    shiny::textInput(ns("db_schema"), "Schema", value = "public"),
-    actionButton(ns("connect_db"), "Connect to Database", style = add_button_theme(), icon = icon("link")),
-    actionButton(ns("disconnect_db"), "Disconnect from Database", style = add_button_theme(), icon = icon("unlink"))
+    width = 12,
+    status = "primary",
+    solidHeader = TRUE,
+    background = "white",
+    fluidPage(
+      fluidRow(
+        column(width = 3, textInput(ns("db_name"), "Database Name", value = Sys.getenv('TH_DATABASE'))),
+        column(width = 3, passwordInput(ns("db_host"), "Host", value = Sys.getenv('TH_HOST'))),
+        column(width = 2, numericInput(ns("db_port"), "Port", value = as.numeric(Sys.getenv('TH_PORT')))),
+        column(width = 3, passwordInput(ns("db_user"), "Username", value = Sys.getenv('TH_DB_USERNAME'))),
+        column(width = 3, passwordInput(ns("db_password"), "Password", value = Sys.getenv('TH_DB_PASSWORD'))),
+        column(width = 2, textInput(ns("db_schema"), "Schema", value = "public"))
+      ),
+      fluidRow(
+        column(width = 3, uiOutput(ns("connect_db_btn"))),
+        column(width = 3, uiOutput(ns("disconnect_db_btn")))
+      )
+    )
   )
 }
 
@@ -33,8 +43,18 @@ dbConfigUI <- function(id) {
 #' @importFrom bs4Dash updateTabItems
 #' @importFrom th2dbm connect_to_database
 dbConfigServer <- function(id, parent_session = NULL) {
-  shiny::moduleServer(id, function(input, output, session) {
-    con <- shiny::reactiveVal(NULL)
+  moduleServer(id, function(input, output, session) {
+    con <- reactiveVal(NULL)
+    ns <- session$ns
+    output$connect_db_btn <- renderUI({
+      req(input$db_name, input$db_host, input$db_user, input$db_password, input$db_port, input$db_schema)
+      actionButton(ns("connect_db"), "Connect to Database", style = add_button_theme(), icon = icon("link"))
+    })
+
+    output$disconnect_db_btn <- renderUI({
+      req(con())
+      actionButton(ns("disconnect_db"), "Disconnect from Database", style = add_button_theme(), icon = icon("unlink"))
+    })
 
     shiny::observeEvent(input$connect_db, {
       db_params <- list(
@@ -46,8 +66,14 @@ dbConfigServer <- function(id, parent_session = NULL) {
       )
 
       tryCatch({
+
         new_con <- th2dbm::connect_to_database(db_params, db_type = "postgresql")
         con(new_con)
+        Sys.setenv(TH_DATABASE = input$db_name)
+        Sys.setenv(TH_HOST = input$db_host)
+        Sys.setenv(TH_PORT = as.character(input$db_port))
+        Sys.setenv(TH_DB_USERNAME = input$db_user)
+        Sys.setenv(TH_DB_PASSWORD = input$db_password)
         shiny::showNotification("Connected to database successfully", type = "message")
         bs4Dash::updateTabItems(parent_session, "sidebar", selected = "tables")
       }, error = function(e) {
--- R/dbm_create_table_mod.R
+++ R/dbm_create_table_mod.R
@@ -11,7 +11,7 @@ createTableUI <- function(id) {
   ns <- NS(id)
   fluidPage(
     bs4Dash::box(
-      title = "Create New Table",width = 12, status = "primary", solidHeader = TRUE, background = "white",
+      title = "Create New Table", width = 12, status = "primary", solidHeader = TRUE, background = "white",
       textInput(ns("table_name"), "Table Name"),
       numericInput(ns("num_columns"), "Number of Columns", value = 1, min = 1),
       uiOutput(ns("column_inputs")),
@@ -36,26 +36,52 @@ createTableServer <- function(id, con, schema, data_changed, parent_session = NU
     ns <- session$ns
 
     column_types <- c(
-      "integer", "bigint", "numeric", "real", "double precision",
-      "char", "varchar", "text", "date", "time", "timestamp",
-      "boolean", "uuid", "json", "Choice"
+      "varchar", "numeric", "text", "date", "time", "timestamp",
+      "boolean", "uuid", "Choice"
     )
+    c_ids <- "COLUMN_IDs.csv"
+    if (file.exists(c_ids)) {
+      file.remove(c_ids)
+    }
 
-    output$column_inputs <- renderUI({
-      num_columns <- input$num_columns
-      lapply(1:num_columns, function(i) {
-        fluidRow(
-          column(4, textInput(ns(paste0("column_name_", i)), paste("Column", i, "Name"))),
-          column(4, selectInput(ns(paste0("column_type_", i)), paste("Column", i, "Type"), choices = column_types)),
-          column(4,
-                 conditionalPanel(
-                   condition = paste0("input['", ns(paste0("column_type_", i)), "'] == 'Choice'"),
-                   textInput(ns(paste0("choice_values_", i)), "Enter possible values (comma-separated)")
-                 )
-          )
-        )
+    # output$column_inputs <- renderUI({
+    #   num_columns <- input$num_columns
+    #   lapply(1:num_columns, function(i) {
+    #     fluidRow(
+    #       column(4, textInput(ns(paste0("column_name_", i)), paste("Column", i, "Name"))),
+    #       column(4, selectizeInput(ns(paste0("column_type_", i)), paste("Column", i, "Type"),
+    #                                choices = column_types,
+    #                                options = list(create = FALSE))),
+    #       column(4,
+    #              conditionalPanel(
+    #                condition = paste0("input['", ns(paste0("column_type_", i)), "'] == 'Choice'"),
+    #                selectizeInput(ns(paste0("choice_values_", i)), "Add choices",
+    #                               choices = NULL,
+    #                               multiple = TRUE,
+    #                               options = list(create = TRUE, plugins = list("remove_button")))
+    #              )
+    #       )
+    #     )
+    #   })
+    # })
+    observeEvent(input$num_columns, {
+      req(input$num_columns, input$table_name)
+      temp <- 1:input$table_num_items
+      lapply(temp, function(i) {
+        new_col_id <- generateID(prefix = glue::glue("{input$table_name}_"))
+        mod_col_bloc_server(paste0("column_", i), tab_name = input$table_name, indice = new_col_id)
       })
     })
+
+    output$columns_id <- renderUI({
+      req(input$num_columns)
+      temp <- 1:input$num_columns
+      columns_id <- lapply(temp, function(i) {
+        mod_col_bloc_ui(ns(paste0("column_", i)))
+      })
+      columns_id
+    })
+
 
     output$create_table <- renderUI({
       req(input$table_name)
@@ -64,35 +90,64 @@ createTableServer <- function(id, con, schema, data_changed, parent_session = NU
 
     observeEvent(input$create_table, {
       req(con())
+      if (!file.exists(c_ids)) {
+        print(glue::glue("Le fichier CSV {c_ids} n'existe pas"))
+        return(NULL)
+      }
+
       table_name <- input$table_name
       num_columns <- input$num_columns
+      tryCatch({
+        search_query <- sprintf("SELECT * FROM th2metadata_table WHERE TH2DB_TABLE = '%s'", table_name)
+        search_query <- DBI::dbGetQuery(con(), search_query)
 
-      columns <- sapply(1:num_columns, function(i) {
+        if (nrow(search_query) != 0 & action == "create") {
+          th2dbm::th_shinyalert(
+            title = glue::glue("{input$table_name} table already exists !"),
+            confirmButtonCol = "#013DFF",
+            text = "",
+            type = "error"
+          )
+          showNotification(glue::glue("{input$table_name} table already exists !"), type = "error")
+          return("table already exists !")
+        }
+      }, error = function(e) {
+        print(paste("Error seaching table:", e$message))
+        showNotification(paste("Error seaching table:", e$message), type = "error")
+      })
+
+      enum_queries <- character(0)
+      columns <- character(0)
+
+      for (i in 1:num_columns) {
         col_name <- input[[paste0("column_name_", i)]]
         col_type <- input[[paste0("column_type_", i)]]
 
         if (col_type == "Choice") {
+          choices_values <- input[[paste0("choice_values_", i)]]
           enum_name <- paste0(table_name, "_", col_name, "_enum")
-          enum_values <- input[[paste0("choice_values_", i)]]
-          enum_query <- paste0("CREATE TYPE ", schema(), ".", enum_name, " AS ENUM (", paste0("'", gsub(",", "','", enum_values), "'"), ")")
-          tryCatch({
-            DatabaseConnector::dbExecute(con(), enum_query)
-          }, error = function(e) {
-            showNotification(paste("Error creating enum:", e$message), type = "error")
-          })
-          paste(col_name, enum_name)
+          enum_query <- paste0("CREATE TYPE ", schema(), ".", enum_name, " AS ENUM (",
+                               paste0("'", choices_values, "'", collapse = ", "), ")")
+          enum_queries <- c(enum_queries, enum_query)
+          columns <- c(columns, paste(col_name, enum_name))
         } else {
-          paste(col_name, col_type)
+          columns <- c(columns, paste(col_name, col_type))
         }
-      })
+      }
 
       columns <- c("colid varchar(255) PRIMARY KEY", columns)
       columns_str <- paste(columns, collapse = ", ")
 
-      query <- paste0("CREATE TABLE ", schema(), ".", table_name, " (", columns_str, ")")
-
       tryCatch({
+        # Create ENUM types
+        for (query in enum_queries) {
+          DatabaseConnector::dbExecute(con(), query)
+        }
+
+        # Create table
+        query <- paste0("CREATE TABLE ", schema(), ".", table_name, " (", columns_str, ")")
         DatabaseConnector::dbExecute(con(), query)
+
         data_changed(data_changed() + 1)
         bs4Dash::updateTabItems(parent_session, "sidebar", selected = "tables")
         showNotification("Table created successfully", type = "message")
