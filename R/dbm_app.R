5842c02e8a1cec35cd963baf1f8bb2ef59066d6f
Fair Analytics <158599661+dgnaglo@users.noreply.github.com>
2024-09-23 16:27:52 +0200

    Update dbm_app.R

--- R/dbm_app.R
+++ R/dbm_app.R
@@ -11,27 +11,26 @@ dbm_ui <- function(request){
         shiny.info::powered_by("thaink²", link = "https://www.thaink2.com/", position = "bottom right"),
         shinybusy::add_busy_bar(color = "#013DFF"),
         includeCSS(system.file("custom_icon.css", package = "SaldaeReporting")),
-    bs4Dash::dashboardPage(
-        header = prepare_app_header(),
-        preloader = list(html = tagList(waiter::spin_clock(), "Loading ..."), color = "#013DFF"),
-        title = "Database Manager & Viewer",
-    bs4Dash::dashboardSidebar(
-        bs4Dash::sidebarMenu(
-            id = "sidebar",
-            bs4Dash::menuItem("Configuration", tabName = "config", icon = icon("cogs")),
-            bs4Dash::menuItem("Tables", tabName = "tables", icon = icon("table")),
-            bs4Dash::menuItem("Create Table", tabName = "create", icon = icon("plus"))
-        )
-    ),
-    bs4Dash::dashboardBody(
-        bs4Dash::tabItems(
-            bs4Dash::tabItem(tabName = "config", dbConfigUI("db_config")),
-            bs4Dash::tabItem(tabName = "tables", tableListUI("table_list")),
-            bs4Dash::tabItem(tabName = "create", createTableUI("create_table"))
+        bs4Dash::dashboardPage(
+            header = prepare_app_header(),
+            preloader = list(html = tagList(waiter::spin_clock(), "Loading ..."), color = "#013DFF"),
+            title = "Database Manager & Viewer",
+            bs4Dash::dashboardSidebar(
+                bs4Dash::sidebarMenu(
+                    id = "sidebar",
+                    bs4Dash::menuItem("Configuration", tabName = "config", icon = icon("cogs")),
+                    uiOutput("dynamic_menu_items")  # Utilisation d'un menu dynamique
+                )
+            ),
+            bs4Dash::dashboardBody(
+                bs4Dash::tabItems(
+                    bs4Dash::tabItem(tabName = "config", dbConfigUI("db_config")),
+                    bs4Dash::tabItem(tabName = "tables", tableListUI("table_list")),
+                    bs4Dash::tabItem(tabName = "create", createTableUI("create_table"))
+                )
+            )
         )
     )
-    )
-)
 }
 
 #' dbm_app_launcher
@@ -39,11 +38,21 @@ dbm_ui <- function(request){
 dbm_server <- function(input, output, session){
 
     db_config <- dbConfigServer("db_config", parent_session = session)
-    Sys.setenv("DB_PASSWD" = "f~3}~dcWK[<=;6Cx4");
     data_changed <- reactiveVal(0)
 
+    # Mise à jour du menu latéral en fonction de la connexion à la base de données
+    output$dynamic_menu_items <- renderUI({
+        print(db_config$con())
+        if (!is.null(db_config$con())) {
+            tagList(
+                bs4Dash::menuItem("Tables", tabName = "tables", icon = icon("table")),
+                bs4Dash::menuItem("Create Table", tabName = "create", icon = icon("plus"))
+            )
+        }
+    })
+
+    createTableServer("create_table", con = db_config$con, schema = db_config$schema, data_changed = data_changed, parent_session = session)
     tableListServer("table_list", db_config$con, db_config$schema, data_changed)
-    createTableServer("create_table", db_config$con, db_config$schema, data_changed, parent_session = session)
 
     # Close the connection at the end of the session
     session$onSessionEnded(function() {
@@ -59,9 +68,9 @@ dbm_app_launcher <- function(target_port = 3838, target_host = '0.0.0.0'){
     if (file.exists("./R/initialize_db_configs.R")) init_envs()
     options(shiny.maxRequestSize = 100000 * 1024^2)
     shinyApp(ui = dbm_ui, server = dbm_server,
-                    options = list(launch.browser = TRUE,
-                                   port = target_port,
-                                   host = target_host,
-                                   java.parameters = "-Xss3m")
-                    )
+             options = list(launch.browser = TRUE,
+                            port = target_port,
+                            host = target_host,
+                            java.parameters = "-Xss3m")
+    )
 }
