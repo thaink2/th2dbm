08a856cfb99c5bc7e31d4a09616ad7611fbecf2c
Farid Azouaou <farid.azouaou@thaink2.com>
2024-11-27 14:47:59 +0100

    make a clean app for jwt

--- R/mod_jwt_toke_gen.R
+++ R/mod_jwt_toke_gen.R
@@ -1,3 +1,29 @@
+#' Prepare the Application Header
+#' Creates a custom header for a Shiny application, including the application logo and links.
+#' @export
+prepare_bi_app_header <- function(app_title = "BI & Reporting", app_logo = NULL, app_logo_circle = NULL,logo_href = NULL) {
+  if(is.null(app_logo_circle))app_logo_circle <- "https://raw.githubusercontent.com/thaink2/thaink2publicimages/main/thaink2_logo_circle.png"
+  if(is.null(app_logo))app_logo <- "https://static.wixstatic.com/media/9aacb8_9886b3a143c2470d96ec76a181e67e49~mv2.png/v1/fill/w_195,h_46,al_c,q_85,usm_0.66_1.00_0.01,enc_auto/thaink2-logo-blue-big.png"
+  if(is.null(logo_href))logo_href <- "https://www.thaink2.com/en"
+  bs4Dash::bs4DashNavbar(
+    skin = "light",
+    title = bs4Dash::bs4DashBrand(
+      title = bs4Dash::bs4DashBrand(title = app_title, image = app_logo_circle)
+    ),
+    rightUi = shiny::tagList(
+      shiny::tags$li(
+        class = "nav-item dropdown",
+        shiny::tags$a(
+          href = logo_href,
+          target = "_blank",
+          class = "nav-link",
+          shiny::tags$img(src = app_logo, style = "height: 30px")
+        )
+      )
+    )
+  )
+}
+
 mod_jwt_gen_ui <- function(id) {
   ns <- NS(id)
   uiOutput(ns("jwt_inputs"))
@@ -11,7 +37,7 @@ mod_jwt_gen_server <- function(id, target_table, mod_refresh_file) {
         column(width = 2, uiOutput(ns("user_mail"))),
         column(width = 2, uiOutput(ns("target_service"))),
         column(width = 3, uiOutput(ns("addition_info"))),
-        column(width = 3, uiOutput(ns("generate_token")))
+        column(width = 3, br(), uiOutput(ns("generate_token")))
       )
     })
 
@@ -26,7 +52,7 @@ mod_jwt_gen_server <- function(id, target_table, mod_refresh_file) {
     })
     output$generate_token <- renderUI({
       req(input$user_mail,input$target_service)
-      actionButton(inputId = ns("generate_token"), label = "Generate", icon = icon("key"))
+      actionButton(inputId = ns("generate_token"), style = add_button_theme(), label = "Generate", icon = icon("key"))
     })
 
     observeEvent(input$generate_token,{
@@ -36,8 +62,10 @@ mod_jwt_gen_server <- function(id, target_table, mod_refresh_file) {
       encrypted_token <- jose::jwt_encode_hmac(claim  = jwt_claim, secret = Sys.getenv("ENCRYPT_PASS"))
       showModal(
         modalDialog(title = "JWT Token Value", size = "xl",
-                    h2(encrypted_token))
-      )
+                    fluidPage(
+                      h6(encrypted_token)
+                    )
+      ))
       Sys.sleep(5)
       removeModal()
     })
@@ -45,17 +73,38 @@ mod_jwt_gen_server <- function(id, target_table, mod_refresh_file) {
   })
 }
 
+add_button_theme <- function(btn_col = "#fff", background_col = "#013DFF", border_col = "#013DFF") {
+  style <- glue::glue("color: {btn_col}; background-color: {background_col}; border-color: {border_col};
+                                border-radius: 10px;
+                               border-width: 2px")
+  return(style)
+}
+
+jwt_app_ui <- function(request){
+  bs4Dash::bs4DashPage(
+    options = list(sidebarExpandOnHover = TRUE),
+    header = prepare_bi_app_header(app_title = "JWT generator"),
+    sidebar = bs4Dash::bs4DashSidebar(
+      id = "sidebar",
+      bs4Dash::bs4SidebarMenu(
+        bs4Dash::bs4SidebarMenuItem(
+          "JWT Generator",
+          icon = icon("key"),
+          tabName = "jwt_generator",
+          startExpanded = TRUE)
+      )
+    ),
+    body = bs4Dash::dashboardBody(
+      mod_jwt_gen_ui("thaink2jwt")
+    )
+  )
+}
 #' launch_jst_gen_app
 #' @export
 launch_jwt_gen_app <- function(){
-  library(shiny)
-  ui <- fluidPage(
-    title = "thaink² services token generator",
-    mod_jwt_gen_ui("test")
-  )
-  server <- function(input, output, session) {
-    mod_jwt_gen_server("test")
+  jwt_app_server <- function(input, output, session){
+    mod_jwt_gen_server("thaink2jwt")
   }
-  shinyApp(ui, server, options = list(launch.browser = T, port = 3838, host = "0.0.0.0"))
+  shiny::shinyApp(ui = jwt_app_ui, server = jwt_app_server, options = list(launch.browser = T, port = 3838, host = "0.0.0.0"))
 }
 
